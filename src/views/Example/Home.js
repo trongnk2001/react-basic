c4a783f2c0c75b8dac3bf91ff151dd0a2e772398
Khac Trong <trongnk2001@gmail.com>
2022-07-19 12:40:26 +0700

    ngay 19/07

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { withRouter } from "react-router";
 class Home extends React.Component {
+    componentDidMount(){
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        },3000)
+    }
     render() {
+        console.log('Check',this.props)
         return (
             <div>Home</div>
         )
     }
 }
-export default Home;
+export default withRouter(Home);
